9b5f04625401b5efafda059206f8011f89842f9d
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2020-10-01 13:50:44 +0800

    add assets/app.js

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+function index() {
+    let app = document.getElementById("app");    
+    let h3 = document.createElement("h3");
+    h3.innerHTML = "Player Times";
+
+    app.appendChild(h3);
+}
+
+index();
