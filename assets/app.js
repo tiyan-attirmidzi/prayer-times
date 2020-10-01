b837f432b331e3686413904e37c995eb57082138
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2020-10-01 14:00:15 +0800

    permission geolocation

--- assets/app.js
+++ assets/app.js
@@ -4,6 +4,24 @@ function index() {
     h3.innerHTML = "Player Times";
 
     app.appendChild(h3);
+
+    visitorLocation();
+}
+
+function visitorLocation() {
+    if (!navigator.geolocation) {
+        alert("GeoLocation tidak didukung didalam browser anda, silahkan gunakan browser lain");
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error);
+    }
+}
+
+function success(position) {
+    console.log(position);
+}
+
+function error() {
+    alert("Lokasi tidak dapat akses");
 }
 
 index();
